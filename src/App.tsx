4d8f07002ec683246c0be5495754699adfc289d9
rawcam <rawcam@ya.ru>
2026-03-28 08:23:06 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Topbar } from './components/layout/Topbar'
+import { Sidebar } from './components/layout/Sidebar'
+import { DashboardPage } from './pages/DashboardPage'
+import { ProjectsPage } from './pages/ProjectsPage'
+import { CalculationsPage } from './pages/CalculationsPage'
+import { TemplatesPage } from './pages/TemplatesPage'
+import './styles/App.css'
+
+function App() {
+  return (
+    <BrowserRouter basename="/sputnik-studio-react">
+      <div className="app">
+        <Topbar />
+        <div className="app-layout">
+          <Sidebar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<DashboardPage />} />
+              <Route path="/projects" element={<ProjectsPage />} />
+              <Route path="/calculations" element={<CalculationsPage />} />
+              <Route path="/templates" element={<TemplatesPage />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
