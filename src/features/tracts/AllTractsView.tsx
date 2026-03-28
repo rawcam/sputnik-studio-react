444e089964ac26be19dc10f085e1e5bd093a3446
rawcam <rawcam@ya.ru>
2026-03-28 08:31:57 +0300

    Create AllTractsView.tsx

---
+++ src/features/tracts/AllTractsView.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { RootState } from '../../store'
+import { setActiveTract, setViewMode } from '../../store/tractsSlice'
+
+export const AllTractsView = () => {
+  const dispatch = useDispatch()
+  const tracts = useSelector((state: RootState) => state.tracts.tracts)
+
+  const handleSelectTract = (id: string) => {
+    dispatch(setActiveTract(id))
+    dispatch(setViewMode('single'))
+  }
+
+  return (
+    <div className="all-tracts-view">
+      <h3>Все тракты</h3>
+      {tracts.length === 0 && <div>Нет трактов. Создайте первый.</div>}
+      <ul>
+        {tracts.map(tract => (
+          <li key={tract.id} onClick={() => handleSelectTract(tract.id)} style={{ cursor: 'pointer' }}>
+            {tract.name} (задержка: {tract.totalLatency} мс)
+          </li>
+        ))}
+      </ul>
+      <button className="btn-primary" onClick={() => dispatch(setViewMode('single'))}>
+        Вернуться к активному тракту
+      </button>
+    </div>
+  )
+}
