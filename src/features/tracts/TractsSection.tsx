17db5b09e9e7bce18fbbd1f75c2b3a7d2ab06b7d
rawcam <rawcam@ya.ru>
2026-03-30 19:41:44 +0300

    Update TractsSection.tsx

--- src/features/tracts/TractsSection.tsx
+++ src/features/tracts/TractsSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../store'
-import { setActiveTract, setViewMode } from '../../store/tractsSlice'
+import { addTract, setActiveTract, setViewMode } from '../../store/tractsSlice'
 
 export const TractsSection: React.FC = () => {
   const dispatch = useDispatch()
@@ -15,16 +15,13 @@ export const TractsSection: React.FC = () => {
 
   const handleNew = () => {
     const newId = Date.now().toString()
-    dispatch({
-      type: 'tracts/addTract',
-      payload: {
-        id: newId,
-        name: `Тракт ${tracts.length + 1}`,
-        sourceDevices: [],
-        sinkDevices: [],
-        networkSwitches: [],
-      }
-    })
+    dispatch(addTract({
+      id: newId,
+      name: `Тракт ${tracts.length + 1}`,
+      sourceDevices: [],
+      sinkDevices: [],
+      networkSwitches: [],
+    }))
     dispatch(setActiveTract(newId))
     dispatch(setViewMode('single'))
   }
