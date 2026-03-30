f390a3931ed04b88d825e244bc94b0f8ef163873
rawcam <rawcam@ya.ru>
2026-03-30 09:26:22 +0300

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,16 +4,16 @@ import { CompanyExpense } from '../store/companyExpensesSlice'
 import { ServiceVisit } from '../store/serviceVisitsSlice'
 
 export class SputnikDB extends Dexie {
-  projects!: Table<Project, string>
-  companyExpenses!: Table<CompanyExpense, string>
-  serviceVisits!: Table<ServiceVisit, string>
+  projects!: Table<Project>
+  companyExpenses!: Table<CompanyExpense>
+  serviceVisits!: Table<ServiceVisit>
 
   constructor() {
     super('SputnikDB')
     this.version(1).stores({
-      projects: 'id, shortId, category, status, startDate, engineer, projectManager',
-      companyExpenses: 'id, date, category, paid',
-      serviceVisits: 'id, projectId, date, status, responsible',
+      projects: 'id, shortId, category, status, engineer, projectManager',
+      companyExpenses: 'id, date, category',
+      serviceVisits: 'id, projectId, date, status',
     })
   }
 }
