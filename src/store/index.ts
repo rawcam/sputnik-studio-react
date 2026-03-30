16ad2f78b3b42c148e1d0669da33e08a95301649
rawcam <rawcam@ya.ru>
2026-03-30 11:50:02 +0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,13 @@
-// ... существующие импорты
+import { configureStore } from '@reduxjs/toolkit'
+import themeReducer from './themeSlice'
+import uiReducer from './uiSlice'
+import videoReducer from './videoSlice'
+import networkReducer from './networkSlice'
+import tractsReducer from './tractsSlice'
+import projectsReducer from './projectsSlice'
+import authReducer from './authSlice'
+import companyExpensesReducer from './companyExpensesSlice'
+import serviceVisitsReducer from './serviceVisitsSlice'
 import ledReducer from './ledSlice'
 import soundReducer from './soundSlice'
 import vcReducer from './vcSlice'
@@ -7,7 +16,15 @@ import powerReducer from './powerSlice'
 
 export const store = configureStore({
   reducer: {
-    // ... существующие
+    theme: themeReducer,
+    ui: uiReducer,
+    video: videoReducer,
+    network: networkReducer,
+    tracts: tractsReducer,
+    projects: projectsReducer,
+    auth: authReducer,
+    companyExpenses: companyExpensesReducer,
+    serviceVisits: serviceVisitsReducer,
     led: ledReducer,
     sound: soundReducer,
     vc: vcReducer,
@@ -15,3 +32,6 @@ export const store = configureStore({
     power: powerReducer,
   },
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
