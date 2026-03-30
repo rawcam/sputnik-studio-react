c04dbd785447e703afdff4d49f25acf29efd8ffe
rawcam <rawcam@ya.ru>
2026-03-30 10:54:50 +0300

    Create RisksWidget.tsx

---
+++ src/components/widgets/RisksWidget.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const RisksWidget: React.FC = () => {
+  return (
+    <div className="widget-card">
+      <div className="widget-header">
+        <i className="fas fa-exclamation-triangle"></i>
+        <h3>Риски</h3>
+      </div>
+      <div className="widget-content">
+        <p>Заглушка. Скоро здесь будут риски.</p>
+      </div>
+    </div>
+  )
+}
