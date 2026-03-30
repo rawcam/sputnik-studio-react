dd74a311cad9ee94eb49d413e0d93a36b6850b68
rawcam <rawcam@ya.ru>
2026-03-30 18:52:43 +0300

    Create PowerStatsSection.tsx

---
+++ src/features/power/PowerStatsSection.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../store'
+
+export const PowerStatsSection: React.FC = () => {
+  const totalPower = useSelector((state: RootState) =>
+    state.tracts.tracts.reduce((sum, t) => sum + t.totalPower, 0)
+  )
+  const totalBTU = Math.round(totalPower * 3.412)
+
+  return (
+    <div className="sidebar-section">
+      <div className="section-header" data-section="powerStats">
+        <i className="fas fa-bolt"></i>
+        <span>ПИТАНИЕ</span>
+        <i className="fas fa-angle-down"></i>
+      </div>
+      <div className="section-content" id="powerStatsContent">
+        <div className="widget">
+          <div className="widget-item">
+            <span className="widget-label"><i className="fas fa-bolt"></i><span> PoE:</span></span>
+            <span className="widget-value">0</span>/<span>0</span> Вт
+          </div>
+          <div className="widget-item">
+            <span className="widget-label"><i className="fas fa-fire"></i><span> Мощность:</span></span>
+            <span className="widget-value">{totalPower}</span> Вт
+          </div>
+          <div className="widget-item">
+            <span className="widget-label"><i className="fas fa-thermometer-half"></i><span> Тепло:</span></span>
+            <span className="widget-value">{totalBTU}</span> BTU/ч
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
