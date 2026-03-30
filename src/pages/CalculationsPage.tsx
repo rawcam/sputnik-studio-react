6e79efbdf9f2afd70a14d6b0b3dfb193a645a9ec
rawcam <rawcam@ya.ru>
2026-03-30 19:11:05 +0300

    Update CalculationsPage.tsx

--- src/pages/CalculationsPage.tsx
+++ src/pages/CalculationsPage.tsx
@@ -1,15 +1,46 @@
-import { useSelector } from 'react-redux'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../store'
+import { setViewMode, setActiveCalculator } from '../store/tractsSlice'
 import { ActiveTract } from '../features/tracts/ActiveTract'
 import { AllTractsView } from '../features/tracts/AllTractsView'
+// Импорты калькуляторов будут добавлены позже, пока заглушки
 import './CalculationsPage.css'
 
-export const CalculationsPage = () => {
+export const CalculationsPage: React.FC = () => {
+  const dispatch = useDispatch()
   const viewMode = useSelector((state: RootState) => state.tracts.viewMode)
+  const activeCalculator = useSelector((state: RootState) => state.tracts.activeCalculator)
+
+  const handleBackToTracts = () => {
+    dispatch(setViewMode('single'))
+    dispatch(setActiveCalculator(null))
+  }
+
+  const renderContent = () => {
+    if (viewMode === 'calculator' && activeCalculator) {
+      // Здесь будем рендерить калькуляторы по activeCalculator
+      return (
+        <div className="calculator-view">
+          <button className="btn-secondary" onClick={handleBackToTracts}>
+            <i className="fas fa-arrow-left"></i> Назад к трактам
+          </button>
+          <div className="calculator-container">
+            {/* Заглушка, позже заменим на реальные компоненты */}
+            <p>Калькулятор {activeCalculator} будет здесь</p>
+          </div>
+        </div>
+      )
+    }
+    if (viewMode === 'all') {
+      return <AllTractsView />
+    }
+    return <ActiveTract />
+  }
 
   return (
     <div className="calculations-page">
-      {viewMode === 'single' ? <ActiveTract /> : <AllTractsView />}
+      {renderContent()}
     </div>
   )
 }
