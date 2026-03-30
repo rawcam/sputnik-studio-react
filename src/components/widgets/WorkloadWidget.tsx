dedac0e9e328a0dce090ef929b94415b36552438
rawcam <rawcam@ya.ru>
2026-03-30 10:54:27 +0300

    Create WorkloadWidget.tsx

---
+++ src/components/widgets/WorkloadWidget.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const WorkloadWidget: React.FC = () => {
+  return (
+    <div className="widget-card">
+      <div className="widget-header">
+        <i className="fas fa-users"></i>
+        <h3>Загрузка сотрудников</h3>
+      </div>
+      <div className="widget-content">
+        <p>Заглушка. Скоро здесь будет аналитика.</p>
+      </div>
+    </div>
+  )
+}
