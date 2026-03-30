51d2c779a8327af2e8c6c270371ce8b6035dd7c6
rawcam <rawcam@ya.ru>
2026-03-30 13:38:30 +0300

    Update Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { RootState } from '../../store'
-import { toggleSidebar } from '../../store/uiSlice'
 import { VideoSection } from '../../features/video/VideoSection'
-import { NetworkSection } from '../../features/network/NetworkSection'
-import { LedSection } from '../../features/led/LedSection'
-import { SoundSection } from '../../features/sound/SoundSection'
-import { VcSection } from '../../features/vc/VcSection'
-import { ErgoSection } from '../../features/ergo/ErgoSection'
-import { PowerSection } from '../../features/power/PowerSection'
-import { TractsSection } from '../../features/tracts/TractsSection'
 import './Sidebar.css'
 
 export const Sidebar: React.FC = () => {
-  const dispatch = useDispatch()
   const collapsed = useSelector((state: RootState) => state.ui.sidebarCollapsed)
   const location = window.location.pathname
 
@@ -26,24 +17,24 @@ export const Sidebar: React.FC = () => {
         <h2>SPUTNIK STUDIO</h2>
         <div className="header-actions">
           <div className="theme-switch" id="themeSwitch"><i className="fas fa-sun"></i></div>
-          <div className="collapse-btn" onClick={() => dispatch(toggleSidebar())}>
+          <div className="collapse-btn" id="collapseSidebarBtn">
             <i className={`fas fa-chevron-${collapsed ? 'right' : 'left'}`}></i>
           </div>
         </div>
       </div>
-
       <div id="sidebarSectionsContainer">
         <VideoSection />
-        <NetworkSection />
-        <LedSection />
-        <SoundSection />
-        <VcSection />
-        <ErgoSection />
-        <PowerSection />
-        <TractsSection />
-        {/* Здесь можно добавить статистику и управление, но для краткости оставим */}
+        {/* Остальные аккордеоны будут добавлены позже */}
+        <div className="sidebar-section">
+          <div className="section-header">
+            <i className="fas fa-cog"></i>
+            <span>Остальные настройки (скоро)</span>
+          </div>
+          <div className="section-content">
+            <p>Содержимое будет добавлено в следующих шагах</p>
+          </div>
+        </div>
       </div>
-
       <div className="sidebar-footer">Sputnik Studio v8.0<br />React Edition</div>
     </div>
   )
