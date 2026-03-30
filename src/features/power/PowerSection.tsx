0cfdf791cce21991186494e22ba0aa52e9b89e8a
rawcam <rawcam@ya.ru>
2026-03-30 12:01:22 +0300

    Update PowerSection.tsx

--- src/features/power/PowerSection.tsx
+++ src/features/power/PowerSection.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '../../store'
-import { setPowerConfig } from '../../store/powerSlice'
 
 export const PowerSection: React.FC = () => {
-  const dispatch = useDispatch()
-  const config = useSelector((state: RootState) => state.power)
-
   return (
     <div className="sidebar-section">
       <div className="section-header" data-section="power">
@@ -14,8 +8,8 @@ export const PowerSection: React.FC = () => {
         <span>ЭНЕРГИЯ</span>
         <i className="fas fa-angle-down"></i>
       </div>
-      <div className="section-content">
-        <button className="btn-primary" id="showPowerCalcBtn">Калькулятор питания</button>
+      <div className="section-content" id="powerContent">
+        <p>Калькулятор питания будет реализован позже</p>
       </div>
     </div>
   )
