3735f995d530a991f60b64ee0f55cf9cfa926b5e
rawcam <rawcam@ya.ru>
2026-03-30 18:35:39 +0300

    Update ErgoSection.tsx

--- src/features/ergo/ErgoSection.tsx
+++ src/features/ergo/ErgoSection.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { RootState } from '../../store'
-// import { setErgoConfig } from '../../store/ergoSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../store'
+import { setErgoConfig } from '../../store/ergoSlice'
 
 export const ErgoSection: React.FC = () => {
-  // const dispatch = useDispatch()
-  // const config = useSelector((state: RootState) => state.ergo)
+  const dispatch = useDispatch()
+  const config = useSelector((state: RootState) => state.ergo)
+
+  const handleChange = (field: string, value: any) => {
+    dispatch(setErgoConfig({ [field]: value }))
+  }
 
   return (
     <div className="sidebar-section">
@@ -14,8 +18,14 @@ export const ErgoSection: React.FC = () => {
         <span>Эргономика ЭКП</span>
         <i className="fas fa-angle-down"></i>
       </div>
-      <div className="section-content" id="ergoContent">
-        <p>Калькулятор эргономики будет реализован позже</p>
+      <div className="section-content">
+        <div className="setting"><label>Ширина экрана (см):</label><input type="number" value={config.screenWidth} onChange={e => handleChange('screenWidth', parseFloat(e.target.value))} /></div>
+        <div className="setting"><label>Высота экрана (см):</label><input type="number" value={config.screenHeight} onChange={e => handleChange('screenHeight', parseFloat(e.target.value))} /></div>
+        <div className="setting"><label>Расстояние до экрана (см):</label><input type="number" value={config.distance} onChange={e => handleChange('distance', parseFloat(e.target.value))} /></div>
+        <div className="result-item">
+          <span className="result-label">Рекомендация</span>
+          <span className="result-value">{config.resultText}</span>
+        </div>
       </div>
     </div>
   )
