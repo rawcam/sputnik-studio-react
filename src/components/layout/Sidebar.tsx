a6e700532e518314e2acb096b92f1f2975367477
rawcam <rawcam@ya.ru>
2026-03-31 08:44:25 +0300

    Update Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,30 +1,36 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useLocation } from 'react-router-dom'
 import { RootState } from '../../store'
-import { SortableSidebar } from './SortableSidebar'
+import { toggleSidebar } from '../../store/uiSlice'
+import { sidebarModules } from '../../config/sidebarModules'
+import { AccordionWrapper } from './AccordionWrapper'
 import './Sidebar.css'
 
 export const Sidebar: React.FC = () => {
   const collapsed = useSelector((state: RootState) => state.ui.sidebarCollapsed)
   const location = useLocation()
+  const dispatch = useDispatch()
   const isCalculations = location.pathname.includes('/calculations')
 
   if (!isCalculations) return null
 
+  const handleCollapse = () => {
+    dispatch(toggleSidebar())
+  }
+
   return (
     <div className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
       <div className="sidebar-header">
-        <h2>SPUTNIK STUDIO</h2>
-        <div className="header-actions">
-          <div className="theme-switch" id="themeSwitch"><i className="fas fa-sun"></i></div>
-          <div className="collapse-btn" id="collapseSidebarBtn">
-            <i className={`fas fa-chevron-${collapsed ? 'right' : 'left'}`}></i>
-          </div>
+        {/* Убираем заголовок и кнопку темы, оставляем только кнопку сворачивания */}
+        <div className="collapse-btn" onClick={handleCollapse}>
+          <i className={`fas fa-chevron-${collapsed ? 'right' : 'left'}`}></i>
         </div>
       </div>
       <div id="sidebarSectionsContainer">
-        <SortableSidebar />
+        {sidebarModules.map(module => (
+          <AccordionWrapper key={module.id} module={module} />
+        ))}
       </div>
       <div className="sidebar-footer">Sputnik Studio v8.0<br />React Edition</div>
     </div>
