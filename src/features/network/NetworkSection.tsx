a90bf90d4a80068f318ffc93d5a2ac0efde6c552
rawcam <rawcam@ya.ru>
2026-03-31 06:55:19 +0300

    Update NetworkSection.tsx

--- src/features/network/NetworkSection.tsx
+++ src/features/network/NetworkSection.tsx
@@ -19,46 +19,44 @@ export const NetworkSection: React.FC = () => {
         <i className="fas fa-angle-down"></i>
       </div>
       <div className="section-content">
-        <div className="network-settings">
-          <div className="setting">
-            <label>Среда передачи:</label>
-            <select value={settings.cable} onChange={e => handleChange('cable', e.target.value)}>
-              <option value="Cat5e">Cat5e</option>
-              <option value="Cat6">Cat6</option>
-              <option value="Cat6a">Cat6a</option>
-              <option value="Cat7">Cat7</option>
-              <option value="Cat8">Cat8</option>
-              <option value="OM3">Оптика OM3</option>
-              <option value="wireless">Беспровод</option>
-            </select>
-          </div>
-          <div className="setting">
-            <label>Multicast:</label>
-            <input type="checkbox" checked={settings.multicast} onChange={e => handleChange('multicast', e.target.checked)} />
-          </div>
-          <div className="setting">
-            <label>QoS:</label>
-            <input type="checkbox" checked={settings.qos} onChange={e => handleChange('qos', e.target.checked)} />
-          </div>
-          <div className="setting">
-            <label>Тип сети:</label>
-            <select value={settings.networkType} onChange={e => handleChange('networkType', e.target.value)}>
-              <option value="managed">Управляемая</option>
-              <option value="unmanaged">Неуправляемая</option>
-            </select>
-          </div>
-          <div className="setting">
-            <label>Синхронизация:</label>
-            <select value={settings.syncProtocol} onChange={e => handleChange('syncProtocol', e.target.value)}>
-              <option value="ptp">PTP</option>
-              <option value="ntp">NTP</option>
-              <option value="none">Нет</option>
-            </select>
-          </div>
-          <div className="setting">
-            <label>Резервирование:</label>
-            <input type="checkbox" checked={settings.redundancy} onChange={e => handleChange('redundancy', e.target.checked)} />
-          </div>
+        <div className="setting">
+          <label>Среда передачи:</label>
+          <select value={settings.cable} onChange={e => handleChange('cable', e.target.value)}>
+            <option value="Cat5e">Cat5e</option>
+            <option value="Cat6">Cat6</option>
+            <option value="Cat6a">Cat6a</option>
+            <option value="Cat7">Cat7</option>
+            <option value="Cat8">Cat8</option>
+            <option value="OM3">Оптика OM3</option>
+            <option value="wireless">Беспровод</option>
+          </select>
+        </div>
+        <div className="setting">
+          <label>Multicast:</label>
+          <input type="checkbox" checked={settings.multicast} onChange={e => handleChange('multicast', e.target.checked)} />
+        </div>
+        <div className="setting">
+          <label>QoS:</label>
+          <input type="checkbox" checked={settings.qos} onChange={e => handleChange('qos', e.target.checked)} />
+        </div>
+        <div className="setting">
+          <label>Тип сети:</label>
+          <select value={settings.networkType} onChange={e => handleChange('networkType', e.target.value)}>
+            <option value="managed">Управляемая</option>
+            <option value="unmanaged">Неуправляемая</option>
+          </select>
+        </div>
+        <div className="setting">
+          <label>Синхронизация:</label>
+          <select value={settings.syncProtocol} onChange={e => handleChange('syncProtocol', e.target.value)}>
+            <option value="ptp">PTP</option>
+            <option value="ntp">NTP</option>
+            <option value="none">Нет</option>
+          </select>
+        </div>
+        <div className="setting">
+          <label>Резервирование:</label>
+          <input type="checkbox" checked={settings.redundancy} onChange={e => handleChange('redundancy', e.target.checked)} />
         </div>
       </div>
     </div>
