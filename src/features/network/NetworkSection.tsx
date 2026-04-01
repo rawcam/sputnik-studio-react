212927c6aa0f5a4414f9dac3343c880d6708bc45
rawcam <rawcam@ya.ru>
2026-04-01 09:05:57 +0300

    Update NetworkSection.tsx

--- src/features/network/NetworkSection.tsx
+++ src/features/network/NetworkSection.tsx
@@ -12,52 +12,45 @@ export const NetworkSection: React.FC = () => {
   }
 
   return (
-    <div className="sidebar-section">
-      <div className="section-header" data-section="network">
-        <i className="fas fa-network-wired"></i>
-        <span>NETWORK</span>
-        <i className="fas fa-angle-down"></i>
+    <div className="section-content-inner">
+      <div className="compact-setting">
+        <label>Среда передачи:</label>
+        <select value={settings.cable} onChange={e => handleChange('cable', e.target.value)}>
+          <option value="Cat5e">Cat5e</option>
+          <option value="Cat6">Cat6</option>
+          <option value="Cat6a">Cat6a</option>
+          <option value="Cat7">Cat7</option>
+          <option value="Cat8">Cat8</option>
+          <option value="OM3">Оптика OM3</option>
+          <option value="wireless">Беспровод</option>
+        </select>
       </div>
-      <div className="section-content">
-        <div className="setting">
-          <label>Среда передачи:</label>
-          <select value={settings.cable} onChange={e => handleChange('cable', e.target.value)}>
-            <option value="Cat5e">Cat5e</option>
-            <option value="Cat6">Cat6</option>
-            <option value="Cat6a">Cat6a</option>
-            <option value="Cat7">Cat7</option>
-            <option value="Cat8">Cat8</option>
-            <option value="OM3">Оптика OM3</option>
-            <option value="wireless">Беспровод</option>
-          </select>
-        </div>
-        <div className="setting">
-          <label>Multicast:</label>
-          <input type="checkbox" checked={settings.multicast} onChange={e => handleChange('multicast', e.target.checked)} />
-        </div>
-        <div className="setting">
-          <label>QoS:</label>
-          <input type="checkbox" checked={settings.qos} onChange={e => handleChange('qos', e.target.checked)} />
-        </div>
-        <div className="setting">
-          <label>Тип сети:</label>
-          <select value={settings.networkType} onChange={e => handleChange('networkType', e.target.value)}>
-            <option value="managed">Управляемая</option>
-            <option value="unmanaged">Неуправляемая</option>
-          </select>
-        </div>
-        <div className="setting">
-          <label>Синхронизация:</label>
-          <select value={settings.syncProtocol} onChange={e => handleChange('syncProtocol', e.target.value)}>
-            <option value="ptp">PTP</option>
-            <option value="ntp">NTP</option>
-            <option value="none">Нет</option>
-          </select>
-        </div>
-        <div className="setting">
-          <label>Резервирование:</label>
-          <input type="checkbox" checked={settings.redundancy} onChange={e => handleChange('redundancy', e.target.checked)} />
-        </div>
+      <div className="compact-setting checkbox">
+        <label>Multicast:</label>
+        <input type="checkbox" checked={settings.multicast} onChange={e => handleChange('multicast', e.target.checked)} />
+      </div>
+      <div className="compact-setting checkbox">
+        <label>QoS:</label>
+        <input type="checkbox" checked={settings.qos} onChange={e => handleChange('qos', e.target.checked)} />
+      </div>
+      <div className="compact-setting">
+        <label>Тип сети:</label>
+        <select value={settings.networkType} onChange={e => handleChange('networkType', e.target.value)}>
+          <option value="managed">Управляемая</option>
+          <option value="unmanaged">Неуправляемая</option>
+        </select>
+      </div>
+      <div className="compact-setting">
+        <label>Синхронизация:</label>
+        <select value={settings.syncProtocol} onChange={e => handleChange('syncProtocol', e.target.value)}>
+          <option value="ptp">PTP</option>
+          <option value="ntp">NTP</option>
+          <option value="none">Нет</option>
+        </select>
+      </div>
+      <div className="compact-setting checkbox">
+        <label>Резервирование:</label>
+        <input type="checkbox" checked={settings.redundancy} onChange={e => handleChange('redundancy', e.target.checked)} />
       </div>
     </div>
   )
