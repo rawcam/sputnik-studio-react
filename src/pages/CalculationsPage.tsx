77d844d606527f5c7d3efacae4391e7c0b7f94fe
rawcam <rawcam@ya.ru>
2026-04-01 08:10:12 +0300

    Update CalculationsPage.tsx

--- src/pages/CalculationsPage.tsx
+++ src/pages/CalculationsPage.tsx
@@ -4,6 +4,8 @@ import { RootState } from '../store'
 import { setViewMode, setActiveCalculator } from '../store/tractsSlice'
 import { ActiveTract } from '../features/tracts/ActiveTract'
 import { AllTractsView } from '../features/tracts/AllTractsView'
+import { LedCalculator } from '../features/led/LedCalculator'
+// аналогично импорты для SoundCalculator, VcCalculator, ErgoCalculator, PowerCalculator – позже добавим
 import './CalculationsPage.css'
 
 export const CalculationsPage: React.FC = () => {
@@ -16,18 +18,31 @@ export const CalculationsPage: React.FC = () => {
     dispatch(setActiveCalculator(null))
   }
 
+  const renderCalculator = () => {
+    switch (activeCalculator) {
+      case 'led':
+        return <LedCalculator />
+      // case 'sound': return <SoundCalculator />
+      // case 'vc': return <VcCalculator />
+      // case 'ergo': return <ErgoCalculator />
+      // case 'power': return <PowerCalculator />
+      default:
+        return (
+          <div className="calculator-view">
+            <button className="btn-secondary" onClick={handleBackToTracts}>
+              <i className="fas fa-arrow-left"></i> Назад к трактам
+            </button>
+            <div className="calculator-container">
+              <p>Калькулятор {activeCalculator} будет здесь</p>
+            </div>
+          </div>
+        )
+    }
+  }
+
   const renderContent = () => {
     if (viewMode === 'calculator' && activeCalculator) {
-      return (
-        <div className="calculator-view">
-          <button className="btn-secondary" onClick={handleBackToTracts}>
-            <i className="fas fa-arrow-left"></i> Назад к трактам
-          </button>
-          <div className="calculator-container">
-            <p>Калькулятор {activeCalculator} будет здесь</p>
-          </div>
-        </div>
-      )
+      return renderCalculator()
     }
     if (viewMode === 'all') {
       return <AllTractsView />
