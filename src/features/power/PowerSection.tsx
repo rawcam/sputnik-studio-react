10bac9faee2da54c7b0ae7cb5b1ab5f2c9fa401a
rawcam <rawcam@ya.ru>
2026-04-01 08:48:15 +0300

    Update PowerSection.tsx

--- src/features/power/PowerSection.tsx
+++ src/features/power/PowerSection.tsx
@@ -11,17 +11,10 @@ export const PowerSection: React.FC = () => {
   }
 
   return (
-    <div className="sidebar-section">
-      <div className="section-header" data-section="power">
-        <i className="fas fa-bolt"></i>
-        <span>ЭНЕРГИЯ</span>
-        <i className="fas fa-angle-down"></i>
-      </div>
-      <div className="section-content">
-        <button className="btn-primary" onClick={openCalculator}>
-          <i className="fas fa-calculator"></i> Калькулятор питания
-        </button>
-      </div>
+    <div className="section-content-inner">
+      <button className="btn-primary" onClick={openCalculator}>
+        <i className="fas fa-calculator"></i> Калькулятор питания
+      </button>
     </div>
   )
 }
