dfe1ee9817688867d7f6821f6175c5bffa99cf6b
rawcam <rawcam@ya.ru>
2026-04-01 08:27:33 +0300

    Update PowerSection.tsx

--- src/features/power/PowerSection.tsx
+++ src/features/power/PowerSection.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '../../store'
-import { setPowerConfig } from '../../store/powerSlice'
+import { useDispatch } from 'react-redux'
+import { setViewMode, setActiveCalculator } from '../../store/tractsSlice'
 
 export const PowerSection: React.FC = () => {
   const dispatch = useDispatch()
-  const config = useSelector((state: RootState) => state.power)
 
-  const handleChange = (field: string, value: any) => {
-    dispatch(setPowerConfig({ [field]: value }))
+  const openCalculator = () => {
+    dispatch(setViewMode('calculator'))
+    dispatch(setActiveCalculator('power'))
   }
 
   return (
@@ -19,12 +18,9 @@ export const PowerSection: React.FC = () => {
         <i className="fas fa-angle-down"></i>
       </div>
       <div className="section-content">
-        <div className="setting"><label>Суммарная мощность оборудования (Вт):</label><input type="number" value={config.totalPower} onChange={e => handleChange('totalPower', parseFloat(e.target.value))} /></div>
-        <div className="setting"><label>Требуемая автономия ИБП (часов):</label><input type="number" step="0.5" value={config.upsAutonomy} onChange={e => handleChange('upsAutonomy', parseFloat(e.target.value))} /></div>
-        <div className="result-item">
-          <span className="result-label">Рекомендация</span>
-          <span className="result-value">{config.resultText}</span>
-        </div>
+        <button className="btn-primary" onClick={openCalculator}>
+          <i className="fas fa-calculator"></i> Калькулятор питания
+        </button>
       </div>
     </div>
   )
