8ee5a4174ec619b8f80039f35d680cff7c181488
rawcam <rawcam@ya.ru>
2026-04-02 08:34:35 +0300

    Update PowerStatsSection.tsx

--- src/features/power/PowerStatsSection.tsx
+++ src/features/power/PowerStatsSection.tsx
@@ -1,26 +1,28 @@
+// PowerStatsSection.tsx
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../store'
 
 export const PowerStatsSection: React.FC = () => {
-  const totalPower = useSelector((state: RootState) =>
-    state.tracts.tracts.reduce((sum, t) => sum + t.totalPower, 0)
-  )
+  const tracts = useSelector((state: RootState) => state.tracts.tracts)
+  const totalPower = tracts.reduce((sum, t) => sum + t.totalPower, 0)
   const totalBTU = Math.round(totalPower * 3.412)
+  const totalPoEBudget = tracts.reduce((sum, t) => sum + t.totalPoEBudget, 0)
+  const usedPoE = tracts.reduce((sum, t) => sum + t.usedPoE, 0)
 
   return (
     <div className="section-content-inner">
       <div className="widget-item">
         <span className="widget-label">PoE:</span>
-        <span className="power-value">0</span>/<span>0</span> Вт
+        <span className="power-value">{usedPoE}</span>/<span>{totalPoEBudget}</span> Вт
       </div>
       <div className="widget-item">
         <span className="widget-label">Мощность:</span>
-        <span className="power-value">{totalPower} Вт</span>
+        <span className="power-value">{totalPower}</span> Вт
       </div>
       <div className="widget-item">
         <span className="widget-label">Тепло:</span>
-        <span className="power-value">{totalBTU} BTU/ч</span>
+        <span className="power-value">{totalBTU}</span> BTU/ч
       </div>
     </div>
   )
