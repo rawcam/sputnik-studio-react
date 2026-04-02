5b32f00b6960f0bdf3b8028cb7c5280dedb9036a
rawcam <rawcam@ya.ru>
2026-04-02 11:27:18 +0300

    Update NetworkStatsSection.tsx

--- src/features/network/NetworkStatsSection.tsx
+++ src/features/network/NetworkStatsSection.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../../store'
-import { calcLoadPercent } from '../../store/networkSlice'
 
 export const NetworkStatsSection: React.FC = () => {
   const network = useSelector((state: RootState) => state.network)
   const tracts = useSelector((state: RootState) => state.tracts.tracts)
-  const totalBitrate = tracts.reduce((sum, t) => sum + t.totalBitrate, 0)
-  const loadPercent = calcLoadPercent(totalBitrate, network.cable)
-  const totalPorts = tracts.reduce((sum, t) => sum + t.totalPorts, 0)
-  const usedPorts = tracts.reduce((sum, t) => sum + t.usedPorts, 0)
+  const activeTractId = useSelector((state: RootState) => state.tracts.activeTractId)
+  const activeTract = tracts.find(t => t.id === activeTractId)
+
+  // Если нет активного тракта, показываем нули
+  const totalBitrate = activeTract?.totalBitrate || 0
+  const loadPercent = activeTract?.networkLoadPercent || 0
+  const totalPorts = activeTract?.totalPorts || 0
+  const usedPorts = activeTract?.usedPorts || 0
 
   return (
     <div className="section-content-inner">
