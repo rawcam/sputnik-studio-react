a31eaf1b1c2535c1229b818a8f9bffd572ed0c1a
rawcam <rawcam@ya.ru>
2026-04-04 13:59:28 +0300

    Update SpecificationPage.tsx

--- src/pages/SpecificationPage.tsx
+++ src/pages/SpecificationPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../store'
 import { SpecificationTable } from '../components/specification/SpecificationTable'
 import { resetSpecification } from '../store/specificationSlice'
 import * as XLSX from 'xlsx'
@@ -7,11 +8,27 @@ import './SpecificationPage.css'
 
 export const SpecificationPage: React.FC = () => {
   const dispatch = useDispatch()
+  const sections = useSelector((state: RootState) => state.specification.sections)
 
   const exportToExcel = () => {
-    // Собираем данные из Redux store (пришлось бы импортировать store, но проще получить через useSelector – но здесь неудобно. Временно сделаем через глобальный объект, но для чистоты можно вынести в отдельный thunk.
-    // Чтобы не усложнять, экспорт будет работать после того, как мы подключим store. Пока сделаем заглушку.
-    alert('Экспорт в Excel будет реализован в следующей версии. Сейчас вы можете сохранить данные через localStorage.')
+    const wsData: any[][] = []
+    wsData.push(['№','Вендор','Модель','Наименование','Кол.','Ед.','USD','EUR','+%','RUB/ед','Сумма RUB','Скидка %','Сумма со скидкой','Поставщик','Срок поставки'])
+    let rowNum = 1
+    sections.forEach(section => {
+      wsData.push([section.name, '', '', '', '', '', '', '', '', '', '', '', '', '', ''])
+      section.rows.forEach(row => {
+        wsData.push([
+          rowNum++, row.vendor, row.model, row.name, row.qty, row.unit,
+          row.usd, row.eur, row.markup, row.rubUnit, row.sum, row.discount, row.sumDisc,
+          row.supplier, row.delivery
+        ])
+      })
+    })
+    wsData.push(['','','','','','','','','','','','','Итого:', sections.reduce((sum,s)=>sum + s.rows.reduce((s2,r)=>s2+r.sumDisc,0),0), ''])
+    const ws = XLSX.utils.aoa_to_sheet(wsData)
+    const wb = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(wb, ws, 'Спецификация')
+    XLSX.writeFile(wb, `specification_${new Date().toISOString().slice(0,19)}.xlsx`)
   }
 
   return (
