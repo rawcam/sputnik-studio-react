691a98391aa0282db406c4beafb1f09113a20df6
rawcam <rawcam@ya.ru>
2026-04-07 13:17:58 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { DashboardPage } from './pages/DashboardPage'
 import { ProjectsPage } from './pages/ProjectsPage'
 import { CalculationsPage } from './pages/CalculationsPage'
 import { SpecificationPage } from './pages/SpecificationPage'
+import { SpecificationsListPage } from './pages/SpecificationsListPage'
 import './styles/global.css'
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
               <Route path="/dashboard" element={<DashboardPage />} />
               <Route path="/projects" element={<ProjectsPage />} />
               <Route path="/calculations" element={<CalculationsPage />} />
+              <Route path="/specifications" element={<SpecificationsListPage />} />
               <Route path="/specification" element={<SpecificationPage />} />
             </Routes>
           </main>
