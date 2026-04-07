fc68c2c66e9f5daf59395d377d99f0041d6310ab
rawcam <rawcam@ya.ru>
2026-04-07 16:33:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-// src/App.tsx
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { Topbar } from './components/layout/Topbar'
-import { DashboardPage } from './pages/DashboardPage'
-import { ProjectsPage } from './pages/ProjectsPage'
-import { CalculationsPage } from './pages/CalculationsPage'
-import { SpecificationPage } from './pages/SpecificationPage'
-import { SpecificationsListPage } from './pages/SpecificationsListPage'
-import './styles/global.css'
 
 function App() {
   return (
     <HashRouter>
-      <div className="app">
-        <Topbar />
-        <div className="app-layout">
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<Navigate to="/dashboard" replace />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
-              <Route path="/projects" element={<ProjectsPage />} />
-              <Route path="/calculations" element={<CalculationsPage />} />
-              <Route path="/specifications" element={<SpecificationsListPage />} />
-              <Route path="/specification/:id" element={<SpecificationPage />} />
-              <Route path="/specification" element={<SpecificationPage />} />
-            </Routes>
-          </main>
-        </div>
+      <div>
+        <h1>Тест</h1>
+        <p>Приложение работает</p>
       </div>
     </HashRouter>
   )
